95605724fe2e946f76cf2e68eff8b6cb655432b8
bshin132 <bshin132@gmail.com>
2022-01-19 06:36:48 +0000

    complete spinner2

---
+++ spinner2.js
@@ -0,0 +1,9 @@
+const spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|\n'];
+let interval = 0;
+
+for(let i = 0; i < spinner.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(`\r${spinner[i]}`)
+    }, interval)
+    interval += 300;
+  } 
